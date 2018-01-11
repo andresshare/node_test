a1f8f41d5e662624805f920557b851328d6c57f0
andresshare <andresshare@gmail.com>
2018-01-11 10:17:21 -0500

    archivos de prueba inicial

--- app.js
+++ app.js
@@ -1,35 +1,23 @@
 var http = require('http')
-var fs =require('fs')
-
-http.createServer(function (req, resp) {
-    console.log("servidor activado");
-    resp.writeHead(200, { 'content-type': 'text/plain' })
-    
+var fs = require('fs')
+http.createServer(function (peticion, respuesta) {
+    respuesta.writeHead(200, {'Content-Type': 'text/html' })
     switch (peticion.url) {
-        case "/": plantilla = "inicio.html"    
-            break;
-        
-        case "/nodejs": plantilla = "sobrenode.html"
-            break;
-
+        case '/':
+            plantilla = "inicio.html"
+            break    
+        case '/nodejs':
+            plantilla = "sobrenode.html"
+            break
+    
         default:
-            plantilla = "404.html"
-            
+            plantilla="404.html"    
             break;
     }
+    fs.readFile("./plantilllas/"+plantilla,function (error,datos) {
+        respuesta.write(datos)
+        respuesta.end()
 
-    fs.readFile("./plantillas/" + plantilla, function (error,datos) {
-        resp.write()
-        resp.end()
-        })
-
-
-}).listen(8080,'localhost')
-
-console.log('el servidor esta en localhost');
-
-
-
-
-
+    })
+}).listen(8080, 'localhost')
 

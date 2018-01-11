c31b51e492ff849689ce7e3e2e3fe55bc3c99afb
andresshare <andresshare@gmail.com>
2018-01-11 15:18:19 -0500

    prueba finalizada

--- app.js
+++ app.js
@@ -1,23 +1,56 @@
-var http = require('http')
-var fs = require('fs')
-http.createServer(function (peticion, respuesta) {
-    respuesta.writeHead(200, {'Content-Type': 'text/html' })
-    switch (peticion.url) {
-        case '/':
-            plantilla = "inicio.html"
-            break    
-        case '/nodejs':
-            plantilla = "sobrenode.html"
-            break
-    
-        default:
-            plantilla="404.html"    
-            break;
-    }
-    fs.readFile("./plantilllas/"+plantilla,function (error,datos) {
-        respuesta.write(datos)
-        respuesta.end()
+var fs= require('fs')
+var express = require("express")
+var app = express()
 
+app.use(express.static(__dirname + '/views'))
+
+//Datos
+
+var camisetasDatos = [
+    {
+        titulo: "Naranja", imagen: "../images/camiseta_uno.jpg",
+        titulo: "Roja", imagen: "../images/camiseta_dos.jpg",
+        titulo: "azul", imagen: "../images/camiseta_tres.jpg",
+        titulo: "verde", imagen: "../images/camiseta_cuatro.jpg",
+     }
+ ]
+
+
+
+app.get('/', function (peticion, respuesta) {
+    respuesta.render('index.jade',
+        {
+            titulo: "tienda de frutas del json",
+            fruta: "guayaba",
+            iamgen:"../smile.png"
+}    )
+})
+
+app.get('/tienda',function (peticion,respuesta) {
+    respuesta.render('tienda.jade', {
+        titulo: "tienda de Camisetas Json",
+        camisetas: camisetasDatos
     })
-}).listen(8080, 'localhost')
 
+})
+
+app.get('/tienda/comprar/:camiseta', function (peticion, respuesta) {
+   
+    var objetos = camisetasDatos.filter(function () {
+        if (peticion.params.camiseta==obj.titulo) {
+        respuesta.render('compra.jade',obj)
+    }        
+    })[0]
+})
+
+app.use(function (peticion,respuesta) {
+    respuesta.status(400)
+    respuesta.render('404.jade',{titulo:'404:pagina no encontrada '})
+})
+
+
+
+app.listen(8080,function () {
+    console.log('escuchando en puerto');
+    
+})
